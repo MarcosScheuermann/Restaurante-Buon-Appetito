7221be385b152193657a2140b46fdb35972e7106
Fer Trujillo <fernandatrujillo.code@gmail.com>
2022-03-28 10:59:01 -0300

    layout

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,5 @@
+const Footer = () => {
+    return (<p>Soy un Footer</p>  );
+}
+ 
+export default Footer;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,5 @@
+const Header =() => {
+    return (<p> Soy un Header</p>);
+}
+
+export default Header;
