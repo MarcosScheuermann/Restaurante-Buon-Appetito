2a0db271162fd45af4a934ed57a5d6e21c0ca4eb
MarcosScheuermann <scheuermannjm@gmail.com>
2022-03-28 22:21:56 -0300

    cambiosfooter

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,13 +1,9 @@
 const Footer = () => {
-<<<<<<< HEAD
     return ( 
         <h1>
             Soy un FOOTER
         </h1>
       );
-=======
-    return ( <p>Footer</p> );
->>>>>>> 6096d4b2be73d6fd377622dfd1f05630498e2728
 }
  
 export default Footer;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { useEffect, useState } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { Link } from "react-router-dom";
@@ -64,44 +63,6 @@ const Header = () => {
         </Navbar.Collapse>
       </Container>
     </MyNavbar>
-=======
-import React from "react";
-import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
-
-
-
-const Header = () => {
-  return (
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-  <Container>
-  <Navbar.Brand href="/" className="buon-logo"><img
-          alt=""
-          src="/img/Logo_BuonAppetito.jpg"
-          width="100"
-          height="100"
-          className="d-inline-block align-top"
-        />Buon Appetito</Navbar.Brand>
-  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-  <Navbar.Collapse id="responsive-navbar-nav">
-    <Nav className="me-auto">
-      <Nav.Link href="/">Página principal</Nav.Link>
-      <Nav.Link href="/Login">Login</Nav.Link>
-      <NavDropdown title="Mas" id="collasible-nav-dropdown">
-        <NavDropdown.Item href="/Productos">Productos</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-        <NavDropdown.Divider />
-        <NavDropdown.Item href="/Contacto">Contacto</NavDropdown.Item>
-        <NavDropdown.Item href="/Admin">Administrador</NavDropdown.Item>
-      </NavDropdown>
-    </Nav>
-    <Nav>
-      <Nav.Link href="/Nosotros">Nosotros</Nav.Link>
-      <Nav.Link href="/Contacto">Contacto</Nav.Link>
-    </Nav>
-  </Navbar.Collapse>
-  </Container>
-</Navbar>
->>>>>>> 6096d4b2be73d6fd377622dfd1f05630498e2728
   );
 };
 
