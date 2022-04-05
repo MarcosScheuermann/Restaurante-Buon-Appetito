529dc4c4465ff49385e18c7f87b593cd59575093
MarcosScheuermann <scheuermannjm@gmail.com>
2022-04-04 23:52:10 -0300

    arreglos de prueba

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ import Home from "./pages/Home";
 import ProductDetail from "./pages/ProductDetail";
 import Products from "./pages/Products";
 import "./App.css";
-import PrivateRoute from "./routes/PrivateRoute";
-import AdminRoute from "./routes/AdminRoute";
-import PublicRoute from "./routes/PublicRoute";
+// import PrivateRoute from "./routes/PrivateRoute";
+// import AdminRoute from "./routes/AdminRoute";
+// import PublicRoute from "./routes/PublicRoute";
 import Login from "./pages/Login";
 import UserProvider from "./context/UserContext";
 import ProductsProvider from "./context/ProductsContext";
@@ -26,6 +26,7 @@ function App() {
                 <Route path="/" element={<Home />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/admin" element={<Admin />} />
+                <Route path="/home" element={<Home />} />
                 <Route path="/contacto" element={<Contacto />} />
                 <Route path="/productos" element={<Products />} />
                 <Route path="/productos/:id" element={<ProductDetail />} />
