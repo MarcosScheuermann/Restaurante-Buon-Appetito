c74a8b2726710fc1769f862a9d1034180d5a38e4
MarcosScheuermann <scheuermannjm@gmail.com>
2022-04-14 18:29:47 -0300

    borron

--- src/assets/img/logo.js
+++
@@ -1,9 +0,0 @@
-import BuonAppetito from "./LogoBuonAppetito.jpg";
-import BuonAppetitoICO from "../Logo/BuonAppetitoICO.jpg";
-import LogoBuonAppetito from "../Logo/BuonAppetito.png";
-
-export default {
-    "img1": BuonAppetito,
-    "img2": BuonAppetitoICO,
-    "img3": LogoBuonAppetito    
-}
--- src/pages/CartPage.js
+++
@@ -1,9 +0,0 @@
-import Carrito from '../components/CarritoCompra/CarritoCompra'
-
-const CartPage = () => {
-    return (
-        <Carrito></Carrito>
-    );
-}
- 
-export default CartPage;
