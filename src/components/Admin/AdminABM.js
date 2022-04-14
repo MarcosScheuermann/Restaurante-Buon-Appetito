54e86d226223454db17afbb9825a649135a9a936
MarcosScheuermann <scheuermannjm@gmail.com>
2022-04-14 09:10:44 -0300

    cambios marcos en ABM

--- src/components/Admin/AdminABM.js
+++ src/components/Admin/AdminABM.js
@@ -8,9 +8,6 @@ import ProductsProvider from "../../context/ProductsContext";
 
 const AdminABM = () => {
   const [products, setProducts] = useState([]);
-
-  // const products = useGet(URL_PRODUCTS);
-
   const getProducts = async () => {
     try {
       const response = await axiosClient.get("/products");
@@ -23,61 +20,10 @@ const AdminABM = () => {
     getProducts();
   },[])
 
-
-
-// const {products} = useContext (ProductsContext);
-
-
-  // const handleDelete = async (e) => {
-  //   const id = e.target.parentElement.parentElement.id;
-  //   await axios.delete(URL_PRODUCTS +"/"+ id)
-  // }
   return (
     <>
       <p>SOY ADMINISTRADOR</p>
-
-      <table>
-        {products.map((product, index) => (
-          <tr>
-            <td>
-              <p key={index}>😭{product.id}</p>
-            </td>
-          </tr>
-        ))}
-      </table>
     </>
-    // <>
-    //   <Container>
-    //     <Button className="mb-2">Alta de Producto</Button>
-    //     <Table striped bordered hover>
-    //         <thead>
-    //             <tr>
-    //                 <th>ID</th>
-    //                 <th>NOMBRE</th>
-    //                 <th>PRECIO</th>
-    //                 <th>IMAGEN</th>
-    //                 <th>ACCIONES</th>
-    //             </tr>
-    //         </thead>
-    //       <tbody>
-    //         {products.map(({ id, name, price, image }) => (
-    //           <tr key={id} id={id}>
-    //             <td>{id}</td>
-    //             <td>{name}</td>
-    //             <td>{price}</td>
-    //             <td>{image}</td>
-    //             <td>
-    //               <Button variant="success" className="w-100 mb-2">Actualizar</Button>
-    //               <Button variant="danger" className="w-100 mb-2" onClick={(e) => handleDelete(e)}>
-    //                 Eliminar
-    //               </Button>
-    //             </td>
-    //           </tr>
-    //         ))}
-    //       </tbody>
-    //     </Table>
-    //   </Container>
-    // </>
   );
 }
 
