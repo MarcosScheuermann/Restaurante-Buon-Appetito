360c834e3b93d63cad412220fe1ef43b09c538fb
MarcosScheuermann <scheuermannjm@gmail.com>
2022-04-15 10:07:23 -0300

    Layout

--- src/components/Layout/layout.js
+++ src/components/Layout/layout.js
@@ -1,13 +1,9 @@
-
-import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
 
 const Layout = ({children}) => {
     return ( 
         <>
-        <Header/>
         {children}
-        <Footer/>
         </>
      );
 }
