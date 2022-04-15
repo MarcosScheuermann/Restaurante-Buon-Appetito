5489c3b5621852de550906f7b5280bc0a83e20ec
Lucas Ferruchi <lucasferruchi@hotmail.es>
2022-04-15 01:53:37 -0300

    limpio Appjs. Correccion de AboutUs.css. Aplicacion de clases en AboutUs.js

--- src/App.js
+++ src/App.js
@@ -6,24 +6,9 @@ import PublicRoute from "./routes/PublicRoute";
 import LandingPage from "./pages/LandingPage";
 import AboutUs from "./pages/AboutUs";
 import Login from "./pages/Login";
-
-import Admin from "./pages/Admin";
-import CartPage from "./pages/CartPage";
-//import ProductDetail from "./pages/ProductDetail";
-import ProductPage from "./pages/ProductPage";
-import Products from "./pages/Products";
-
-
 import ProductsProvider from "./context/ProductsContext";
-//import Register from "./pages/Register";
-//import Contacto from "./pages/Contact";
-//import AdminABM from "./components/Admin/AdminABM";
-// import ProductsState from "./context/products/ProductsState";
-//import Home from "./pages/Home";
-//import Nosotros from "./pages/AboutUs";
-// import ProductDetail from "./pages/ProductDetail";
-// import AdminRoute from "./routes/AdminRoute";
-// import AdminRoute from "./routes/AdminRoute";
+
+
 
 
 
@@ -41,17 +26,9 @@ function App() {
               <Route path="/" element={<PublicRoute><LandingPage /></PublicRoute>} />
               <Route path="/AboutUs" element={<PublicRoute><AboutUs /></PublicRoute>} />
               <Route path="/login" element={<PublicRoute><Login /></PublicRoute>} />
-              {/*<Route path="/nosotros" element={<Nosotros />} />*/}
+            
              
-              {/*<Route path="/registro" element={<Register />} />*/}
-              <Route path="/admin" element={<Admin />} />
-              {/*<Route path="/contacto" element={<Contacto />} />*/}
-              <Route path="/producto" element={<ProductPage/>} />
-              <Route path='/carrito' element={<CartPage/>} />
-              <Route path="/productos" element={<Products />} />
-              {/*<Route path="/home" element={<PrivateRoute><Home /></PrivateRoute>} />*/}
-              {/*<Route path="/productos" element={<Productos />} /> */}
-              {/* <Route path="/productos/:id" element={<ProductDetail />} /> */}
+              
             </Routes>
         </Layout>
         </ProductsProvider>
--- src/components/AboutUs/AboutUs.js
+++ src/components/AboutUs/AboutUs.js
@@ -1,71 +1,74 @@
 import "./AboutUs.css";
-import { Container} from "react-bootstrap";
-import LucasImg from "./../../assets/Integrantes/Lucas Ferruchi.jpeg"
-import GastonImg from "./../../assets/Integrantes/Gastón Devalis.jpg"
-import JuanImg from "./../../assets/Integrantes/Juan Ernesto Villegas.jpg"
-import FerImg from "./../../assets/Integrantes/Maria Fernanda Trujillo.jpg"
-import MarcosImg from "./../../assets/Integrantes/Marcos Scheuermann.jpeg"
-
+import { Container } from "react-bootstrap";
+import LucasImg from "./../../assets/Integrantes/Lucas Ferruchi.jpeg";
+import GastonImg from "./../../assets/Integrantes/Gastón Devalis.jpg";
+import JuanImg from "./../../assets/Integrantes/Juan Ernesto Villegas.jpg";
+import FerImg from "./../../assets/Integrantes/Maria Fernanda Trujillo.jpg";
+import MarcosImg from "./../../assets/Integrantes/Marcos Scheuermann.jpeg";
 
 const AboutUs = () => {
-    return (
-        <Container>
-        <h2>Diseñadores de Buon Appetito</h2>
+  return (
+    <Container>
+      
+        <h2 className="us-title">Diseñadores de Buon Appetito</h2>
         <div className="content-all">
-            <div className="content-carrousel"> 
-                  
-                <figure><img src={LucasImg}/>
-                    <div className="image__overlay image__overlay--primary">
-                        <div className="image__title">Ferruchi, Lucas</div>
-                        <p className="image__description">
-                            Hobby: <br/>
-                            Hacer deportes
-                        </p>
-                    </div>
-                </figure>
-                <figure><img src={MarcosImg}/>
-                    <div className="image__overlay image__overlay--primary">
-                        <div className="image__title">Scheuermann, Marcos</div>
-                        <p className="image__description">
-                            Hobby: <br/>
-                            Pasear a Bruno
-                        </p>
-                    </div>
-                </figure>
-                <figure><img src={FerImg}/>
-                    <div className="image__overlay image__overlay--primary">
-                        <div className="image__title">Trujillo, M. Fernanda</div>
-                        <p className="image__description">
-                            Hobby: <br/>
-                            Trekking
-                        </p>
-                    </div>                
-                </figure>
-                
-                <figure><img src={JuanImg}/>
-                <div className="image__overlay image__overlay--primary">
-                    <div className="image__title">Villegas, Juan Ernesto</div>
-                    <p className="image__description">
-                        Hobby: <br/>
-                        Fútbol - Trekking
-                    </p>
-                </div>
-                </figure>   
-                <figure><img src={GastonImg}/>
-                    <div className="image__overlay image__overlay--primary">
-                        <div className="image__title">Devails, Gastón</div>
-                        <p className="image__description">
-                            Hobby: <br/>
-                            Hacer deportes
-                        </p>
-                    </div>
-                </figure>
-                
-            </div>
-        </div> 
-    </Container> 
+          <div className="content-carrousel">
+            <figure>
+              <img src={LucasImg} />
+              <div className="image__overlay image__overlay--primary">
+                <div className="image__title">Ferruchi, Lucas</div>
+                <p className="image__description">
+                  Hobby: <br />
+                  Hacer deportes
+                </p>
+              </div>
+            </figure>
+            <figure>
+              <img src={MarcosImg} />
+              <div className="image__overlay image__overlay--primary">
+                <div className="image__title">Scheuermann, Marcos</div>
+                <p className="image__description">
+                  Hobby: <br />
+                  Pasear a Bruno
+                </p>
+              </div>
+            </figure>
+            <figure>
+              <img src={FerImg} />
+              <div className="image__overlay image__overlay--primary">
+                <div className="image__title">Trujillo, M. Fernanda</div>
+                <p className="image__description">
+                  Hobby: <br />
+                  Trekking
+                </p>
+              </div>
+            </figure>
 
-);
-}
- 
+            <figure>
+              <img src={JuanImg} />
+              <div className="image__overlay image__overlay--primary">
+                <div className="image__title">Villegas, Juan Ernesto</div>
+                <p className="image__description">
+                  Hobby: <br />
+                  Fútbol - Trekking
+                </p>
+              </div>
+            </figure>
+            <figure>
+              <img src={GastonImg} />
+              <div className="image__overlay image__overlay--primary">
+                <div className="image__title">Devails, Gastón</div>
+                <p className="image__description">
+                  Hobby: <br />
+                  Hacer deportes
+                </p>
+              </div>
+            </figure>
+          </div>
+        </div>
+      
+    </Container>
+  );
+};
+
 export default AboutUs;
