6df52b645d0efd4bf4820d9609fc9acb5ae3e8cc
MarcosScheuermann <scheuermannjm@gmail.com>
2022-04-15 09:34:23 -0300

    odioaLayout

--- src/components/Layout/layout.js
+++ src/components/Layout/layout.js
@@ -1,8 +1,13 @@
+
+import Footer from "../Footer/Footer";
+import Header from "../Header/Header";
+
 const Layout = ({children}) => {
     return ( 
         <>
+        <Header/>
         {children}
-
+        <Footer/>
         </>
      );
 }
