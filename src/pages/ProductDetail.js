3cb844e2f9570df873d010d99e7726668dd72f9b
MarcosScheuermann <scheuermannjm@gmail.com>
2022-04-18 20:43:06 -0300

    arrglos en detalle de productos

--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -1,10 +1,14 @@
 import { useParams } from "react-router";
 import { useState, useEffect } from "react";
 import axiosClient from "../config/axiosClient";
+import { Col, Container, Row } from "react-bootstrap";
+import ProductCard from "../components/ProductCard/ProductCard";
+import Products from "./Products";
 
 
 const ProductDetail = () => {
     const [product, setProduct] = useState ({})
+    const [products, setProducts] = useState([])
     const params = useParams();
     const id = params.id;
     const getProduct = async () =>{
@@ -27,6 +31,21 @@ console.log(product);
         <p>{product.name}</p>
         <p>{product.description}</p>
         <p>NADA</p>
+        {/* <Container className="mt-5">
+          <Row xs={1} md={3} className="g-2">
+            {product.map((product) => (
+              <Col>
+                <ProductCard
+                  className="m-3"
+                  id={product.id}
+                  title={product.name}
+                  description={product.description}
+                  image={product.image}
+                />
+              </Col>
+            ))}
+          </Row>
+        </Container> */}
       </>
     );
 }
