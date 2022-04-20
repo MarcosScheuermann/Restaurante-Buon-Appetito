d768cd0fbc34b0067eefc830624d6028bf0fd999
MarcosScheuermann <scheuermannjm@gmail.com>
2022-04-19 21:06:32 -0300

    imagen

--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -1,6 +1,7 @@
 import { Card, Button } from "react-bootstrap";
 import './ProductCard.css';
 import {Link} from "react-router-dom";
+import chef from "../../assets/LandingImg/chef.jpg";
 import pizza from "../../assets/LandingImg/pizza.jpg";
 
 
@@ -8,10 +9,10 @@ import pizza from "../../assets/LandingImg/pizza.jpg";
 
 
 
-const ProductCard = ({id,title,description,price}) => {
+const ProductCard = ({id,title,description,price,imageSrc}) => {
   return (
     <Card id={id} text="center" border="success" style={{ width: "18rem" }}>
-      <Card.Img variant="top" className="card-top" imageSrc={pizza} />
+      <Card.Img variant="top" className="" src="https://tse1.mm.bing.net/th?id=OIP.k0jfqTlpfqzH10Mgyn8CrQHaE7&pid=Api&P=0&w=276&h=184" />
       <Card.Body>
         <Card.Title>{title}</Card.Title>
         <Card.Text>{description}</Card.Text>
